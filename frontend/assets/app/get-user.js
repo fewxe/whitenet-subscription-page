489aca2de97335366eaddc2223bbec650722c618
Shutov447 <vanasutov793@gmail.com>
2026-07-03 06:44:34 +0400

    feat: error handle

--- frontend/assets/app/get-user.js
+++ frontend/assets/app/get-user.js
@@ -4,6 +4,20 @@ export default async function getUser() {
 
   const uuid = location.pathname.split("/")[1];
   const userDataRes = await fetch(`/api/sub/${uuid}/info`);
+
+  if (!userDataRes.ok) {
+    const bodyElem = document.getElementsByTagName("body")[0];
+    const status = userDataRes.status || 404;
+    const message = userDataRes.statusText || "Unknown Error";
+    console.log(bodyElem, status, message);
+    bodyElem.innerHTML = `
+                          Error: <br>
+                          status: ${status} <br>
+                          massage: ${message}
+                         `;
+    return;
+  }
+
   const userData = await userDataRes.json();
   userDataCache = userData;
   return userData;
